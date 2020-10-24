cc85a876ce25ec2ad64e53a7699c0614f346d746
kumvinod <41529852+kumvinod@users.noreply.github.com>
2020-10-24 19:06:40 -0400

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,6 +2,6 @@ def call(Map stageParams){
 	checkout([
 		$class:'GitSCM',
 		branches:[[name:stageParams.branch]],
-		useRemoteConfigs:[[url:stageParams.url]]
+		userRemoteConfigs:[[url:stageParams.url]]
 	])
 }
