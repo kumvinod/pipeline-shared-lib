aa71440c2825b0485874494c5dbc09717d617af3
kumvinod <41529852+kumvinod@users.noreply.github.com>
2020-10-24 17:02:15 -0400

    Update execBuildOrDeploy.groovy

--- vars/execBuildOrDeploy.groovy
+++ vars/execBuildOrDeploy.groovy
@@ -1,50 +1,13 @@
+#!/usr/bin/env groovy
+
 def call(String command) {
 	if (command == "build") 
-	{
-		pipeline {
-			agent any
-			stages 
-			{
-				stage("Build") 
-				{
-					steps 
-					{
-						echo "Build is executed"
-					}
-				}	
-			}
-		}
+	{	
+		echo "Build is executed"
+			
 	}
-	else if(command == "deploy")
+	else if (command == "deploy")
 	{
-		pipeline {
-			agent any
-			stages 
-			{
-				stage("Deploy") 
-				{
-					steps 
-					{
-						echo "Deploy is executed"
-					}
-				}	
-			}
-		}	
-	}
-	else
-	{
-		pipeline {
-			agent any
-			stages 
-			{
-				stage("Do Nothing") 
-				{
-					steps 
-					{
-						echo "Doing nothing"
-					}
-				}	
-			}
-		}		
+		echo "deploy is executed"				
 	}
 }
